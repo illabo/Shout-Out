c979ad350afe6a61813135f4a1a7aa25dd1d58c7
Ilya <uin153974748@gmail.com>
2022-06-28 19:41:04 +0300

    Initial Commit

---
+++ Shout Out/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shout Out
+//
+//  Created by Yachin Ilya on 28.06.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shout Out/Shout_OutApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shout_OutApp.swift
+//  Shout Out
+//
+//  Created by Yachin Ilya on 28.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct Shout_OutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
